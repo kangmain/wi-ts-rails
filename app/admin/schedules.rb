d485ca8bb71438c8d2327589c1fa95e41410cd2e
kangmain <charles@zoyi.co>
2018-01-19 14:25:30 +0900

    schedules time form change

--- app/admin/schedules.rb
+++ app/admin/schedules.rb
@@ -1,13 +1,16 @@
 ActiveAdmin.register Schedule do
-  permit_params :name, :date, :status, :time, :address, :phone_number,:member,:install_type
+  permit_params :name, :category, :date, :status, :time, :address, :phone_number,:member,:install_type
 
   index do
     selectable_column
     id_column
     column :name
+    column :category
     column :date
     column :status
-    column :time
+    column :time do |obj|
+      obj.time.strftime("%H:%M")
+    end
     column :address
     column "Phone Number" do |phone|
       number_to_phone(phone.phone_number, pattern: /(\d{0,2})(\d{3,4})(\d{4})$/, area_code: true, country_code: 82)
@@ -21,9 +24,12 @@ ActiveAdmin.register Schedule do
   show do
     attributes_table do
     row :name
+    row :category
     row :date
     row :status
-    row :time
+    row :time do |obj|
+      obj.time.strftime("%H:%M")
+      end
     row :address
     row :phone_number
     row :member
@@ -46,6 +52,7 @@ ActiveAdmin.register Schedule do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :category
       f.input :date
       f.input :time
       f.input :address
